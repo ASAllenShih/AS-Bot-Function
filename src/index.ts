530a2386581c6ba18577ff425a72b8185f80df3f
AS_Allen_Shih <asallenshih@gmail.com>
2024-10-08 18:43:26 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import {
 	CooldownType,
 	CommandType,
+	CommandConfigJSON,
 	CommandConfig,
 } from './command/config';
 export {
 	CooldownType,
 	CommandType,
+	CommandConfigJSON,
 	CommandConfig,
 }
